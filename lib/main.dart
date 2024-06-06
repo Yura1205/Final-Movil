c5844eb036af929a0a1e8acde759469568607f50
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2024-06-05 21:35:07 -0500

    Correciones a la interfaz Gráfica y el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_final/conexion.dart';
 
-void main() {
-  runApp(ConversionApp());
-}
 
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      brightness: Brightness.dark,
+    ),
+    home: ConversionApp(),
+  ));
+}
